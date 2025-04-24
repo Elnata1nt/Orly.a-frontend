da4407d06bf8c451bc00271826e8e3badeeb49f8
elnatacorrea <elnatacorrea@hotmail.com>
2025-04-23 22:02:56 -0400

    atualizando o sign-up

--- app/(auth)/sign-up/components/sign-up.tsx
+++ app/(auth)/sign-up/components/sign-up.tsx
@@ -1,8 +1,49 @@
+import SignUpForm from "@/components/auth/sign-up/components/sign-up";
+import Image from "next/image";
+
 export default function SignUp() {
   return (
-    <div>
-      <h1>Sign Up</h1>
-      <p>Welcome to the sign-up page!</p>
+    <div className="flex min-h-screen">
+      {/* Left Section */}
+      <div className="relative hidden w-1/2 p-2 lg:block">
+        <div className="h-full w-2/3 overflow-hidden rounded-[40px] bg-gradient-to-b from-indigo-400 via-indigo-600 to-black">
+          <div className="flex h-full flex-col items-center justify-center px-8 text-center text-white">
+            <div className="mb-8">
+              <h1 className="text-2xl font-semibold">
+                Controle Financeiro Inteligente
+              </h1>
+            </div>
+            <h2 className="mb-12 text-8xl font-black">Orly.a</h2>
+            {/* <p className="mb-12 text-lg font-thin">
+              finance
+            </p> */}
+
+            <div className="w-full max-w-sm">
+              <div className="rounded-lg mb-2 bg-white/10 p-4 backdrop-blur-sm">
+                <div className="flex items-center gap-4">
+                  <span className="flex h-8 w-8 items-center justify-center rounded-full bg-white text-black">
+                    1
+                  </span>
+                  <span className="text-lg">Cadastre sua conta</span>
+                </div>
+              </div>
+              <div className="rounded-lg mb-2 bg-white/5 p-4 backdrop-blur-sm">
+                <div className="flex items-center gap-4">
+                  <span className="flex h-8 w-8 items-center justify-center rounded-full bg-white/20 text-white">
+                    2
+                  </span>
+                  <span className="text-lg">Verificação de 2 fatores</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Right Section */}
+      <div className="w-full lg:w-1/3">
+        <SignUpForm />
+      </div>
     </div>
   );
 }
---
+++ components/auth/sign-up/components/sign-up.tsx
@@ -0,0 +1,96 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { DatePickerDemo } from "./data-de-nascimento";
+
+export default function SignUpForm() {
+  return (
+    <div className="flex min-h-svh">
+      <div className="flex w-full items-center justify-center p-6 lg:w-6/6">
+        <div className="mx-auto max-w-sm">
+          <div className="flex flex-col items-center gap-2 text-center">
+            <h1 className="text-2xl text-indigo-500 font-bold">
+              Cadastre sua conta
+            </h1>
+            <p className="text-balance mb-8 text-sm text-muted-foreground">
+              Insira seus dados abaixo para criar sua conta
+            </p>
+          </div>
+          <form className="space-y-6">
+            <div className="space-y-2">
+              <Input
+                className="border  placeholder:text-gray-400"
+                placeholder="Nome Completo"
+                type="text"
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Input
+                className="border  placeholder:text-gray-400"
+                placeholder="m@example.com"
+                type="email"
+              />
+            </div>
+
+            <div className="flex gap-4">
+              <div className="w-1/2">
+                <Input
+                  className="border placeholder:text-gray-400"
+                  placeholder="UF"
+                  type="text"
+                />
+              </div>
+              <div className="w-1/2">
+                <Input
+                  className="border placeholder:text-gray-400"
+                  placeholder="Município"
+                  type="text"
+                />
+              </div>
+            </div>
+
+            <div className="flex gap-4">
+              <div className="w-1/2">
+                <DatePickerDemo />
+              </div>
+              <div className="w-1/2">
+                <Input
+                  className="border placeholder:text-gray-400"
+                  placeholder="(99) 99999-9999"
+                  type="tel"
+                  pattern="\(\d{2}\) \d{5}-\d{4}"
+                />
+              </div>
+            </div>
+
+            <div className="space-y-2">
+              <Input
+                className="border  placeholder:text-gray-400"
+                placeholder="Sua Senha"
+                type="password"
+              />
+            </div>
+            <div className="space-y-2">
+              <Input
+                className="border  placeholder:text-gray-400"
+                placeholder="Confirmar Senha"
+                type="password"
+              />
+            </div>
+
+            <Button className="w-full text-white rounded-md bg-gradient-to-r from-indigo-500 to-indigo-700 hover:bg-gray-100">
+              Sign Up
+            </Button>
+
+            <p className="text-center text-sm">
+              Já tem uma conta?{" "}
+              <a href="/sign-in" className="text-indigo-400 hover:underline">
+                Entrar
+              </a>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
